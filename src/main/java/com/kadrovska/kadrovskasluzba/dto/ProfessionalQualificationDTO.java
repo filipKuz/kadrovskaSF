c37b5bb83931fe3ad005121faf2c08ef2237f4d6
Filip Kuzmanovic <filipkuzmanovic94@gmail.com>
2017-12-08 22:45:54 +0100

    updated project structure and DTOs added

---
+++ src/main/java/com/kadrovska/kadrovskasluzba/dto/ProfessionalQualificationDTO.java
@@ -0,0 +1,53 @@
+package com.kadrovska.kadrovskasluzba.dto;
+
+
+public class ProfessionalQualificationDTO {
+
+	private Long pQId;
+	private String name;
+	private String qualificationDegree;
+	
+	
+	public ProfessionalQualificationDTO() {
+	}
+
+
+	public ProfessionalQualificationDTO(Long pQId, String name, String qualificationDegree) {
+		super();
+		this.pQId = pQId;
+		this.name = name;
+		this.qualificationDegree = qualificationDegree;
+	}
+
+
+	public Long getpQId() {
+		return pQId;
+	}
+
+
+	public void setpQId(Long pQId) {
+		this.pQId = pQId;
+	}
+
+
+	public String getName() {
+		return name;
+	}
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+	public String getQualificationDegree() {
+		return qualificationDegree;
+	}
+
+
+	public void setQualificationDegree(String qualificationDegree) {
+		this.qualificationDegree = qualificationDegree;
+	}
+	
+	
+}
---
+++ src/main/java/com/kadrovska/kadrovskasluzba/services/EmployeeServiceInterface.java
@@ -0,0 +1,5 @@
+package com.kadrovska.kadrovskasluzba.services;
+
+public interface EmployeeServiceInterface {
+
+}
