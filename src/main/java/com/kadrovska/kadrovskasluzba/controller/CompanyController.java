073a0a5953204f1653a1023587b161d5538d59c2
Uros Teofanovic <uteofanovic4@gmail.com>
2018-01-22 16:49:41 +0100

    Added a company controller method

--- src/main/java/com/kadrovska/kadrovskasluzba/controller/CompanyController.java
+++ src/main/java/com/kadrovska/kadrovskasluzba/controller/CompanyController.java
@@ -8,11 +8,15 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.kadrovska.kadrovskasluzba.converter.CompanyDTOToCompany;
 import com.kadrovska.kadrovskasluzba.converter.CompanyToCompanyDTO;
 import com.kadrovska.kadrovskasluzba.dto.CompanyDTO;
+import com.kadrovska.kadrovskasluzba.model.Company;
+import com.kadrovska.kadrovskasluzba.serviceInterfaces.CityServiceInterface;
 import com.kadrovska.kadrovskasluzba.serviceInterfaces.CompanyServiceInterface;
 
 @Controller
@@ -21,6 +25,9 @@ public class CompanyController {
 
 	@Autowired
 	CompanyServiceInterface companyService;
+
+	@Autowired
+	CityServiceInterface cityService;
 
 	@Autowired
 	CompanyDTOToCompany companyDTOToCompanyConverter;
@@ -43,4 +50,27 @@ public class CompanyController {
 		return new ResponseEntity<>(companyToCompanyDTOConverter.convert(companyService.findByIsOursTrue()),
 				HttpStatus.OK);
 	}
+
+	@PutMapping(value = "{id}")
+	public ResponseEntity<?> editCompany(@RequestBody CompanyDTO companyDTO, @PathVariable("id") Long id) {
+		Company company = companyService.findOne(id);
+		if (company == null) {
+			return new ResponseEntity<String>("Can't find company with given id", HttpStatus.BAD_REQUEST);
+		}
+		company.setName(companyDTO.getName());
+		company.setVat(companyDTO.getVat());
+		if (companyDTO.getEmail() != "" && companyDTO.getEmail() != null) {
+			company.setEmail(companyDTO.getEmail());
+		}
+		if (companyDTO.getPhoneNumber() != "" && companyDTO.getPhoneNumber() != null) {
+			company.setPhoneNumber(companyDTO.getPhoneNumber());
+		}
+		if (companyDTO.getAddress() != "" && companyDTO.getAddress() != null) {
+			company.setAddress(companyDTO.getAddress());
+		}
+		company.setCity(cityService.findOne(companyDTO.getCityId()));
+		companyService.save(company);
+
+		return new ResponseEntity<CompanyDTO>(companyToCompanyDTOConverter.convert(company), HttpStatus.OK);
+	};
 }
