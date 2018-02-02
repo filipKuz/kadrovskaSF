d982f6be8d77145af153f050a46b8b68f6242cbd
Vladimir Ilic <vladimirilic96@hotmail.com>
2018-02-02 16:03:22 +0100

    izvestaj

--- src/main/java/com/kadrovska/kadrovskasluzba/controller/AnnualHolidayRegulationController.java
+++ src/main/java/com/kadrovska/kadrovskasluzba/controller/AnnualHolidayRegulationController.java
@@ -2,9 +2,14 @@ package com.kadrovska.kadrovskasluzba.controller;
 
 import java.time.Year;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -13,6 +18,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.view.jasperreports.JasperReportsPdfView;
 
 import com.kadrovska.kadrovskasluzba.converter.AHRDTOtoAHR;
 import com.kadrovska.kadrovskasluzba.converter.AHRtoAHRDTO;
@@ -21,6 +28,10 @@ import com.kadrovska.kadrovskasluzba.model.AnnualHolidayRegulation;
 import com.kadrovska.kadrovskasluzba.model.Employee;
 import com.kadrovska.kadrovskasluzba.serviceInterfaces.AnnualHolidayRegulationServiceInterface;
 import com.kadrovska.kadrovskasluzba.serviceInterfaces.EmployeeServiceInterface;
+
+import net.sf.jasperreports.engine.JREmptyDataSource;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperPrint;
 
 @Controller
 @RequestMapping("/api/annualHolidayRegulations")
@@ -37,7 +48,13 @@ public class AnnualHolidayRegulationController {
 
 	@Autowired
 	private EmployeeServiceInterface employeeService;
-
+	
+	/*
+	@Autowired 
+	@Qualifier("AHRReport1")
+	private JasperReportsPdfView ahrReport;
+	*/
+	
 	Integer thisYear = Year.now().getValue();
 
 	@GetMapping
@@ -53,7 +70,7 @@ public class AnnualHolidayRegulationController {
 				toAhrDTO.convert(annualHolidayRegulationService.findOne(id)), HttpStatus.OK);
 	}
 
-	@GetMapping(value = "findByEmployee/{employeeId}")
+	@GetMapping(value = "/findByEmployee/{employeeId}")
 	public ResponseEntity<List<AnnualHolidayRegulationDTO>> getAHRByEmployeeId(
 			@PathVariable("employeeId") Long employeeId) {
 
@@ -61,9 +78,26 @@ public class AnnualHolidayRegulationController {
 				(toAhrDTO.convert(
 						annualHolidayRegulationService.findByEmployeeEmployeeIdAndBusinessYear(employeeId, thisYear))),
 				HttpStatus.OK);
-
+	}
+	
+	/*
+	@Bean
+	@Qualifier("AHRReport1")
+	public JasperReportsPdfView getHelloWorldReport() {
+	  JasperReportsPdfView v = new JasperReportsPdfView();
+	  v.setUrl("classpath:reports/Blank_A4.jasper");
+	  v.setReportDataKey("datasource");
+	  return v;
 	}
 
+	@GetMapping(value = "AHRReport1")
+	public ModelAndView getRpt2(ModelAndView modelAndView) {
+		  Map<String, Object> parameterMap = new HashMap<>();
+		  parameterMap.put("datasource", new JREmptyDataSource());
+		  modelAndView = new ModelAndView(ahrReport, parameterMap);
+		  return modelAndView;
+		}
+	*/
 	@PostMapping
 	public ResponseEntity<AnnualHolidayRegulationDTO> saveAnnualHolidayRegulation(
 			@RequestBody AnnualHolidayRegulationDTO annualHolidayRegulationDTO) {
@@ -105,4 +139,5 @@ public class AnnualHolidayRegulationController {
 		}
 		return new ResponseEntity<>(annualHolidayRegulationsDTO , HttpStatus.CREATED);
 	}
+	
 }
