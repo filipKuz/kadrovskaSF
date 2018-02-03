849a2c56aa4bed3d9490479d94efb3f1fcc19d18
vulovicm <vulovicmiljan@hotmail.com>
2018-02-03 21:08:24 +0100

    workHistoryController method fixed

--- src/main/java/com/kadrovska/kadrovskasluzba/controller/WorkHistoryController.java
+++ src/main/java/com/kadrovska/kadrovskasluzba/controller/WorkHistoryController.java
@@ -1,12 +1,18 @@
 package com.kadrovska.kadrovskasluzba.controller;
 
-import java.sql.Date;
+import java.text.ParseException;
+import java.time.LocalDate;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.datetime.joda.LocalDateTimeParser;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.Errors;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,11 +21,20 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.ibm.icu.text.DateFormat;
+import com.ibm.icu.text.SimpleDateFormat;
 import com.kadrovska.kadrovskasluzba.converter.WorkHistoryDTOtoWorkHistory;
 import com.kadrovska.kadrovskasluzba.converter.WorkHistoryToWorkHistoryDTO;
+import com.kadrovska.kadrovskasluzba.dto.VacationRequestDTO;
 import com.kadrovska.kadrovskasluzba.dto.WorkHistoryDTO;
+import com.kadrovska.kadrovskasluzba.model.AnnualHolidayRegulation;
+import com.kadrovska.kadrovskasluzba.model.Employee;
+import com.kadrovska.kadrovskasluzba.model.VacationRequest;
 import com.kadrovska.kadrovskasluzba.model.WorkHistory;
+import com.kadrovska.kadrovskasluzba.model.WorkPlace;
+import com.kadrovska.kadrovskasluzba.serviceInterfaces.EmployeeServiceInterface;
 import com.kadrovska.kadrovskasluzba.serviceInterfaces.WorkHistoryServiceInterface;
+import com.kadrovska.kadrovskasluzba.services.WorkPlaceService;
 
 @Controller
 @RequestMapping("/api/workHistory")
@@ -33,6 +48,12 @@ public class WorkHistoryController {
 	
 	@Autowired
 	private WorkHistoryServiceInterface wHistoryService;
+	
+	@Autowired
+	private EmployeeServiceInterface employeeService;
+	
+	@Autowired
+	private WorkPlaceService workPlaceService;
 	
 	// get work history
 	@GetMapping
@@ -64,32 +85,65 @@ public class WorkHistoryController {
 		return new ResponseEntity<>(wHToWhDTO.convert(wHistoryService.findOne(id)), HttpStatus.OK);
 	}
 
-	// new work history
+	
+	
 	@PostMapping(consumes = "application/json")
-	public ResponseEntity<WorkHistory> saveWorkHistory(@RequestBody WorkHistoryDTO newWorkHistory ) {
-
-		WorkHistory wh = wHDTOtoWh.convert(newWorkHistory);
+	public ResponseEntity<?> createVReq(@Validated @RequestBody WorkHistoryDTO workHistoryDTO, Errors errors) {
 		
-		WorkHistory workHistory = wHistoryService.save(wh);
+		if(errors.hasErrors()) {
+			return new ResponseEntity<String>(errors.getAllErrors().toString(), HttpStatus.BAD_REQUEST);
+			}
 		
-		List<WorkHistory> allWh= wHistoryService.findAll();
+		WorkHistory workHistory = new WorkHistory();
 		
-		if(allWh.size() > 0) {
-			WorkHistory lastWorkHistory = allWh.get(0);
-			lastWorkHistory.setEndDate(new Date(System.currentTimeMillis()));
-			wHistoryService.save(lastWorkHistory);
+		workHistory.setWorkHistoryId(workHistoryDTO.getWorkHistoryId());
+		workHistory.setCompanyName(workHistoryDTO.getPreviousCompany());
+		workHistory.setStartDate(workHistoryDTO.getStartDate());
+		workHistory.setEndDate(workHistoryDTO.getEndDate());
+		
+		Employee employee = employeeService.findOne(workHistoryDTO.getEmployeeId());
+		if (employee != null) {
+			workHistory.setEmployee(employee);
+		}
+		
+		if(workHistoryDTO.getWorkPlaceId() != null) {
+			WorkPlace workPlace = workPlaceService.findOne(workHistoryDTO.getWorkPlaceId());
+			if (workPlace != null) {
+				workHistory.setWorkPlace(workPlace);
+			}
 		}
 		
 		
 		
+		Calendar calendar = Calendar.getInstance();
+		Date currentDate = calendar.getTime();
+		java.sql.Date date = new java.sql.Date(currentDate.getTime());
 		
-		return new ResponseEntity("success", HttpStatus.CREATED);
+		//wHistoryService.save(workHistory);
+		
+		List<WorkHistory> allWh= wHistoryService.findAll();
+		
+		WorkHistory lastWorkHistory = wHistoryService.findByEmployeeEmployeeIdAndEndDateIsNull(workHistory.getEmployee().getEmployeeId());
+		if (lastWorkHistory != null) {
+			lastWorkHistory.setEndDate(date);
+		}
+		
+	
+		wHistoryService.save(lastWorkHistory);
+		wHistoryService.save(workHistory);
+		
+		return new ResponseEntity<WorkHistoryDTO>(wHToWhDTO.convert(workHistory), HttpStatus.OK);
+		
 	}
+
 	
 		
 	// edit work history
 	@PutMapping(value="{id}")
-	public ResponseEntity<?> editWorkHistory(@RequestBody WorkHistoryDTO workHistory, @PathVariable("id") Long id){
+	public ResponseEntity<?> editWorkHistory(@Validated @RequestBody WorkHistoryDTO workHistory, @PathVariable("id") Long id, Errors errors){
+		if(errors.hasErrors()) {
+			return new ResponseEntity<String>(errors.getAllErrors().toString(), HttpStatus.BAD_REQUEST);
+			}
 		System.out.println("----------------------------------------------------------");
 		System.out.println("usoo");
 		System.out.println("----------------------------------------------------------");
@@ -121,4 +175,36 @@ public class WorkHistoryController {
 		return 	new ResponseEntity<String> ("Success", HttpStatus.OK);
 	
 	}
+	
+
+//	// new work history
+//	@PostMapping(consumes = "application/json")
+//	public ResponseEntity<?> saveWorkHistory(@Validated @RequestBody WorkHistoryDTO newWorkHistory, Errors errors ) {
+//		if(errors.hasErrors()) {
+//			return new ResponseEntity<String>(errors.getAllErrors().toString(), HttpStatus.BAD_REQUEST);
+//			}
+//
+//		WorkHistory wh = wHDTOtoWh.convert(newWorkHistory);
+//		
+//		WorkHistory workHistory = wHistoryService.save(wh);
+//		
+//		List<WorkHistory> allWh= wHistoryService.findAll();
+//		
+//		if(allWh.size() > 0) {
+//			WorkHistory lastWorkHistory = allWh.get(0);
+//			lastWorkHistory.setEndDate(new Date(System.currentTimeMillis()));
+//			wHistoryService.save(lastWorkHistory);
+//		}
+//		
+//		return new ResponseEntity("success", HttpStatus.CREATED);
+//	}
+	
+	//		if(allWh.size() > 0) {
+//	WorkHistory lastWorkHistory = wHistoryService.findByEmployeeEmployeeIdAndEndDateIsNull(workHistory.getEmployee().getEmployeeId());
+//	if (lastWorkHistory != null) {
+//		lastWorkHistory.setEndDate(date);
+//		wHistoryService.save(lastWorkHistory);
+//	}
+//	
+//	//wHistoryService.save(lastWorkHistory);
 }
