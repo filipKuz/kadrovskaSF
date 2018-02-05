eac87a01d831821e46eaab13d2ddd4e236deab2b
Uros Teofanovic <uteofanovic4@gmail.com>
2018-02-05 15:03:40 +0100

    Added put, post and delete methods in children controller

--- src/main/java/com/kadrovska/kadrovskasluzba/controller/EmployeeChildrenController.java
+++ src/main/java/com/kadrovska/kadrovskasluzba/controller/EmployeeChildrenController.java
@@ -1,19 +1,27 @@
 package com.kadrovska.kadrovskasluzba.controller;
 
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.kadrovska.kadrovskasluzba.converter.EmployeeChildDTOToEmployeeChild;
 import com.kadrovska.kadrovskasluzba.converter.EmployeeChildToEmployeeChildDTO;
 import com.kadrovska.kadrovskasluzba.dto.EmployeeChildDTO;
+import com.kadrovska.kadrovskasluzba.model.EmployeeChild;
 import com.kadrovska.kadrovskasluzba.serviceInterfaces.EmployeeChildServiceInterface;
+import com.kadrovska.kadrovskasluzba.serviceInterfaces.EmployeeServiceInterface;
 
 @Controller
 @RequestMapping("api/children")
@@ -21,6 +29,9 @@ public class EmployeeChildrenController {
 
 	@Autowired
 	EmployeeChildServiceInterface childrenService;
+
+	@Autowired
+	EmployeeServiceInterface employeeService;
 
 	@Autowired
 	EmployeeChildDTOToEmployeeChild childDTOToChildConverter;
@@ -43,7 +54,40 @@ public class EmployeeChildrenController {
 		return new ResponseEntity<>(childToChildDTOConverter.convert(childrenService.findByEmployeeId(employeeId)),
 				HttpStatus.OK);
 	}
-	
-	
 
+	@PostMapping(consumes = "application/json")
+	public ResponseEntity<EmployeeChildDTO> saveChild(@RequestBody EmployeeChildDTO childDTO) {
+
+		EmployeeChild child = childrenService.save(childDTOToChildConverter.convert(childDTO));
+
+		return new ResponseEntity<>(childToChildDTOConverter.convert(child), HttpStatus.OK);
+	}
+
+	@PutMapping(value = "{id}")
+	public ResponseEntity<?> editChild(@RequestBody EmployeeChildDTO childDTO, @PathVariable("id") Long id) {
+		EmployeeChild child = childrenService.findOne(id);
+		if (child == null) {
+			return new ResponseEntity<String>("Can't find child with given id", HttpStatus.BAD_REQUEST);
+		}
+
+		child.setName(childDTO.getName());
+		child.setLastName(childDTO.getLastName());
+		child.setSex(childDTO.getSex());
+		child.setBirthDate(childDTO.getBirthDate());
+		child.setParent(employeeService.findOne(childDTO.getParentId()));
+
+		childrenService.save(child);
+
+		return new ResponseEntity<EmployeeChildDTO>(childToChildDTOConverter.convert(child), HttpStatus.OK);
+	};
+
+	@DeleteMapping(value = "{id}")
+	public ResponseEntity<String> deleteChild(@PathVariable("id") Long id) {
+		EmployeeChild child = childrenService.findOne(id);
+		if (child == null) {
+			return new ResponseEntity<String>("Can't find child with given id", HttpStatus.BAD_REQUEST);
+		}
+		childrenService.delete(id);
+		return new ResponseEntity<String>("Success", HttpStatus.OK);
+	}
 }
